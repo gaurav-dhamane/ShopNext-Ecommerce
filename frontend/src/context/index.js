dde78cb24cb571cdb22e68be8d1bdcf504161d26
Gaurav Dhamane <75262183+gaurav-dhamane@users.noreply.github.com>
2024-05-14 01:05:59 +0530

    Update index.js

--- frontend/src/context/index.js
+++ frontend/src/context/index.js
@@ -1,7 +1,5 @@
 import { createContext } from "react";
 
-const Context = createContext({
-  dispatch: () => {}, // Default dispatch function
-});
+const Context = createContext(null)
 
-export default Context;
+export default Context
