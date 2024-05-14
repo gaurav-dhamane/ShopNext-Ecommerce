941cc815ac5c3177255a088cdb5a232e7997be00
Gaurav Dhamane <75262183+gaurav-dhamane@users.noreply.github.com>
2024-05-14 12:39:30 +0530

    Update userDetails.js

--- backend/controller/user/userDetails.js
+++ backend/controller/user/userDetails.js
@@ -2,7 +2,7 @@ const userModel = require("../../models/userModel")
 
 async function userDetailsController(req,res){
     try{
-        console.log("userId",req.userId)
+        console.log("userId-()",req.userId)
         const user = await userModel.findById(req.userId)
 
         res.status(200).json({
@@ -16,7 +16,7 @@ async function userDetailsController(req,res){
 
     }catch(err){
         res.status(400).json({
-            message : "there is error in userdetail",
+            message : err.message || err,
             error : true,
             success : false
         })
